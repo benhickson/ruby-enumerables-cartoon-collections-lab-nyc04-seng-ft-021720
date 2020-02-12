a9a582a547189b0c18882699a0f930c4ea4e21e4
Ben Hickson <bzhickson@gmail.com>
2020-02-12 02:26:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   array.map{|word| "#{word.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  
+def long_planeteer_calls(array)
+  array.find{|i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
